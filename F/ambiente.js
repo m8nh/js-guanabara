e3ecfcd5793ba7cd3625145a7c750bc102516936
minh <minhnghiemquangminh75@gmail.com>
2022-10-25 14:15:53 +0700

    .

---
+++ F/ambiente.js
@@ -0,0 +1,8 @@
+let valores = [5,8,4,6,11,7]
+//for (let pos=0; pos < valores.length; pos ++){
+ //   console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+//}
+
+//for (let pos in valores){
+  //  console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+//}
